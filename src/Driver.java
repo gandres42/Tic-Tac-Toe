9d341f26a84c50b56c2f8e173446cb1fe68cc56a
gandres42 <49346234+gandres42@users.noreply.github.com>
2020-12-24 12:35:59 -0600

    0 Players and Further Testing
    -One player changed to computer starts to test algorithm with first move
    -Number of players 0 has more A E S T H E T I C  in the linux terminal.

--- src/Driver.java
+++ src/Driver.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
  * Just a basic tic-tac-toe frontend to allow number of player choice, from 2 to 0
  */
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        normalGame();
+    }
+
+    public static void normalGame() throws InterruptedException {
         Board game = new Board();
         Complayer joshua = new Complayer();
         Scanner inout = new Scanner(System.in);
@@ -12,8 +16,10 @@ public class Driver {
         System.out.println("Enter number of players");
         int players = inout.nextInt();
 
-
-        game.printAll();
+        if (players == 0)
+        {
+            WarGames(1000);
+        }
 
         while (game.won() == '.')
         {
@@ -31,22 +37,12 @@ public class Driver {
             }
             else if (players == 1)
             {
-                game.play(inout.nextInt() - 1);
-                if (game.won() == '.')
-                {
-                    game.play(joshua.getPlayIndex(game));
-                }
-                else
-                {
-                    break;
-                }
-            }
-            else if (players == 0)
-            {
                 game.play(joshua.getPlayIndex(game));
+                game.printAll();
                 if (game.won() == '.')
                 {
-                    game.play(joshua.getPlayIndex(game));
+
+                    game.play(inout.nextInt() - 1);
                 }
                 else
                 {
@@ -54,9 +50,9 @@ public class Driver {
                 }
             }
             game.printAll();
-            System.out.println();
         }
 
+        System.out.println();
         game.printAll();
 
         if (game.won() == 'c')
@@ -67,5 +63,43 @@ public class Driver {
         {
             System.out.println("Winner: " + game.won());
         }
+    }
+
+    public static void WarGames(int time) throws InterruptedException {
+        Board game = new Board();
+        Complayer joshua = new Complayer();
+
+        System.out.print("\033[H\033[2J");
+        game.printAll();
+
+        while (game.won() == '.')
+        {
+            game.play(joshua.getPlayIndex(game));
+            System.out.print("\033[H\033[2J");
+            game.printAll();
+            Thread.sleep(time);
+            if (game.won() == '.')
+            {
+                game.play(joshua.getPlayIndex(game));
+            }
+            else
+            {
+                break;
+            }
+            System.out.print("\033[H\033[2J");
+            game.printAll();
+            Thread.sleep(time);
+        }
+
+        if (game.won() == 'c')
+        {
+            System.out.println("Winner: none");
+        }
+        else
+        {
+            System.out.println("Winner: " + game.won());
+        }
+        Thread.sleep(Math.max(time, 20));
+        WarGames((time / 4) * 3);
     }
 }
